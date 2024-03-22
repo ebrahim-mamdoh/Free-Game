64947bed56c6516ee49ad19ef72684a7ac3b6fa0
Ebrahim mamdouh <138860098+ebrahim-mamdoh@users.noreply.github.com>
2024-03-22 05:24:56 +0200

    handel api

--- js/home.module.js
+++ js/home.module.js
@@ -4,7 +4,28 @@ export class Home{
         link.addEventListener('click',()=>{
             document.querySelector('.navbar-nav .active').classList.remove('active');
             link.classList.add('active')
+
+          const category=link.dataset.category
+            console.log(category)
+            this.getGame(category)
         })
       })
     };
+
+
+
+    //function to get the api from ajx
+  async  getGame(cat){
+    const options = {
+      method: 'GET',
+      headers: {
+        'X-RapidAPI-Key': '614ceb4784msh3af123fcef4bc71p1cc86fjsn440153362fe7',
+        'X-RapidAPI-Host': 'free-to-play-games-database.p.rapidapi.com'
+      }
+    };
+
+    const api =await fetch(`https://free-to-play-games-database.p.rapidapi.com/api/games?category=${cat}`,options )
+    const response =await api.json();
+    console.log(response);
+    }
 }
