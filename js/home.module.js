11f5cf804f8d62d2ad9fad949505c45567b623fa
Ebrahim mamdouh <138860098+ebrahim-mamdoh@users.noreply.github.com>
2024-03-24 05:19:04 +0200

    Home style

--- js/home.module.js
+++ js/home.module.js
@@ -5,49 +5,54 @@ export class Home {
     document.querySelectorAll('.nav-link').forEach((link) => {
       link.addEventListener('click', () => {
         this.changeActiveLink(link);
+        const category = link.dataset.category
+        console.log(category)
+       this.displayDataGame(category)
       })
     })
 
     this.ui = new Ui()
+    this.getGame('sailing')
+
   };
 
   async changeActiveLink(link) {
     document.querySelector('.navbar-nav .active').classList.remove('active');
     link.classList.add('active')
 
-    const category = link.dataset.category
-    console.log(category)
+
     // this.getGame(category)
-    const categoryData = await this.getGame(category);
-    this.ui.displayDataGame(categoryData)
+
   }
 
 
-  //function to get the api from ajx
+
   async getGame(cat) {
     try {
-        const options = {
-            method: 'GET',
-            headers: {
-                'X-RapidAPI-Key': '614ceb4784msh3af123fcef4bc71p1cc86fjsn440153362fe7',
-                'X-RapidAPI-Host': 'free-to-play-games-database.p.rapidapi.com'
-            }
-        };
-
-        const api = await fetch(`https://free-to-play-games-database.p.rapidapi.com/api/games?category=${cat}`, options);
-        const response = await api.json();
-        
-        // Check if response is valid
-        if (response && Array.isArray(response)) {
-            return response;
-        } else {
-            throw new Error('Invalid API response');
+      const options = {
+        method: 'GET',
+        headers: {
+          'X-RapidAPI-Key': '614ceb4784msh3af123fcef4bc71p1cc86fjsn440153362fe7',
+          'X-RapidAPI-Host': 'free-to-play-games-database.p.rapidapi.com'
         }
+      };
+
+      const api = await fetch(`https://free-to-play-games-database.p.rapidapi.com/api/games?category=${cat}`, options);
+      const response = await api.json();
+      this.ui.displayDataGame(response)
+
+      // Check if response is valid
+      if (response && Array.isArray(response)) {
+        return response;
+      } else {
+        throw new Error('Invalid API response');
+      }
     } catch (error) {
-        console.error('Error fetching game data:', error);
-        return []; // Return an empty array or handle the error as appropriate
+      console.error('Error fetching game data:', error);
+      return []; // Return an empty array or handle the error as appropriate
     }
-}
+
+  }
 
 }
 
@@ -62,11 +67,3 @@ export class Home {
 
 
 
-
-// try {
-//   const response = await fetch(url, options);
-//   const result = await response.text();
-//   console.log(result);
-// } catch (error) {
-//   console.error(error);
-// }
