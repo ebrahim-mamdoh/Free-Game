19ef5cf8642078b2b5ceda112a749d73c56436e0
Ebrahim mamdouh <138860098+ebrahim-mamdoh@users.noreply.github.com>
2024-03-26 13:26:09 +0200

    add loading screen to details

--- js/details.module.js
+++ js/details.module.js
@@ -3,29 +3,31 @@ import { Ui } from "./ui.module.js";
 
 
 export class Details {
-   constructor(id) {
-     document.getElementById('btnClose').addEventListener('click', () => {
-       document.getElementById('details').classList.add('d-none');
-       document.getElementById('home').classList.remove('d-none');
-     });
- 
-     this.getDetails(id);
-   }
- 
-   async getDetails(id) {
-     const options = {
-       method: 'GET',
-       headers: {
-         'X-RapidAPI-Key': '614ceb4784msh3af123fcef4bc71p1cc86fjsn440153362fe7',
-         'X-RapidAPI-Host': 'free-to-play-games-database.p.rapidapi.com'
-       }
-     };
- 
-     const api = await fetch(`https://free-to-play-games-database.p.rapidapi.com/api/game?id=${id}`, options);
-     const response = await api.json();
-     console.log(response);
-     new Ui().displayDetails(response);
-     
-   }
- }
- 
+  constructor(id) {
+    document.getElementById('btnClose').addEventListener('click', () => {
+      document.getElementById('details').classList.add('d-none');
+      document.getElementById('home').classList.remove('d-none');
+    });
+    this.loading = document.querySelector('.loading')
+    this.getDetails(id);
+  }
+
+  async getDetails(id) {
+this.loading.classList.remove('d-none')
+    const options = {
+      method: 'GET',
+      headers: {
+        'X-RapidAPI-Key': '614ceb4784msh3af123fcef4bc71p1cc86fjsn440153362fe7',
+        'X-RapidAPI-Host': 'free-to-play-games-database.p.rapidapi.com'
+      }
+    };
+
+    const api = await fetch(`https://free-to-play-games-database.p.rapidapi.com/api/game?id=${id}`, options);
+    const response = await api.json();
+    console.log(response);
+    new Ui().displayDetails(response);
+this.loading.classList.add('d-none')
+
+
+  }
+}
